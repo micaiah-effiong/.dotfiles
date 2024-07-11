28e58802fc6e3a5b33e39622649325629f396949
Micah Effiong <micaiah.effiong@gmail.com>
2024-07-11 22:50:58 +0100

    fix bash error

--- zsh/.local/bin/goto.sh
+++ zsh/.local/bin/goto.sh
@@ -5,8 +5,7 @@ function goto {
   SEARCH_DIR=$1
   GOTO_DIR="${SEARCH_DIR:-$(pwd)}"
 
-  if ! command -v fzf &> /dev/null
-  then
+  if ! command -v fzf &> /dev/null; then
       echo "fzf could not be found"
       return
   fi
@@ -14,8 +13,7 @@ function goto {
   # find and select directory with fzf
   SELECTED_DIR=$(find $GOTO_DIR -type d -print | fzf)
 
-  if [ -z "${SELECTED_DIR}" ]; then 
-  else 
+  if ! [ -z "${SELECTED_DIR}" ]; then 
     cd $SELECTED_DIR
   fi
 }
